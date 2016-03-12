d00b67175ad2ed3755e50cdd6354896de992d451
Heather <campbell.hf@gmail.com>
2016-03-12 19:05:25 +0000

    Can filter by completed and dynamically add new filters.

--- js/controllers/listController.js
+++ js/controllers/listController.js
@@ -1,22 +1,42 @@
 toDoList.controller('ListController', ['$resource', function($resource) {
 
   var self = this;
-  this.itemList = { "items": [ ] };
+  this.itemList = { "items": [
+                      {'task': 'go for a run fat bitch', 'completed': false},
+                      {'task': 'sign contract', 'completed': true},
+                      {'task': 'dry clean coat', 'completed': true}
+                    ]
+                  };
 
-  this.getItems = function() {
+
+  this.filters = [
+    {name: 'Active', filterExp: {completed: false}},
+    {name: 'Completed', filerExp: {completed: true}}
+  ];
+
+  this.setFilter = function(filter) {
+    self.selectedFilter = filter;
+    console.log(filter);
   };
 
-  this.addItems = function(item) {
-    console.log(item);
+  this.addItems = function(task) {
+    var item = {'task':task, 'completed':false}
     self.itemList.items.push(item);
-    console.log(self.itemList);
   };
 
   this.removeItem = function(item) {
     var index = self.itemList.items.indexOf(item);
-    console.log(index);
     self.itemList.items.splice(index, 1)
 
+  };
+
+  this.toggleCompleted = function(item) {
+    if (item.completed === false) {
+      item.completed = true;
+    }
+    else {
+      item.completed = false;
+    }
   };
 
 
